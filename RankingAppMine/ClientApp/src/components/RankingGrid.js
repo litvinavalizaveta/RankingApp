0994c72f1bb6a4dbbd7badfb5d78123dbd0abf09
Lizaveta Litvinava <litvinavalizaveta@gmail.com>
2023-10-09 09:17:43 +0300

    Добавьте файлы проекта.

---
+++ RankingAppMine/ClientApp/src/components/RankingGrid.js
@@ -0,0 +1,79 @@
+﻿const RankingGrid = ({ items, imgArr, drag, allowDrop, drop }) => {
+    const rankingGrid = [];
+    const cellCollectionTop = [];
+    const cellCollectionMiddle = [];
+    const cellCollectionBottom = [];
+    const cellCollectionWorst = [];
+
+    function pushCellMarkupToArr(cellCollection, rankNum, rowLabel) { // put div with or without image to each cell of the row
+        if (rankNum > 0) {
+            var item = items.find(o => o.ranking === rankNum);
+            cellCollection.push(<div id={`rank-${rankNum}`} onDrop={drop} onDragOver={allowDrop} className="rank-cell" draggable="true">
+                {(item != null) ? <img id={`item-${item.id}`} src={imgArr.find(o => o.id === item.imageId)?.image} draggable="true" onDragStart={drag} />
+                    : null}
+            </div>);
+        }
+        else {
+            cellCollection.push(<div className="row-label">
+                <h4>{rowLabel}</h4>
+            </div>);
+        }
+    }
+
+    function createCellsForRow(rowNum) {
+        var rankNum = 0;
+        var currCollection = [];
+        var label = "";
+        const numCells = 5;
+
+        for (var a = 1; a <= numCells; a++) {
+            rankNum = (a === 1) ? 0 : (numCells * (rowNum - 1)) + a - rowNum;
+
+            if (rowNum === 1) {
+                currCollection = cellCollectionTop;
+                label = "Top Tier";
+            }
+            else if (rowNum === 2) {
+                currCollection = cellCollectionMiddle;
+                label = "Middle Tier";
+            }
+            else if (rowNum === 3) {
+                currCollection = cellCollectionBottom;
+                label = "Bottom Tier";
+            }
+            else if (rowNum === 4) {
+                currCollection = cellCollectionWorst;
+                label = "Worst Tier";
+            }
+            pushCellMarkupToArr(currCollection, rankNum, label); // fill cells with divs
+        }
+    }
+
+    function createCellsForRows() { // create each cell for a particular row
+        const maxRows = 4;
+        for (var row = 1; row <= maxRows; row++) {
+            createCellsForRow(row);
+        }
+    }
+
+    function createRowsForGrid() { // create each row
+        rankingGrid.push(<div className="rank-row top-tier">{cellCollectionTop}</div>);
+        rankingGrid.push(<div className="rank-row middle-tier">{cellCollectionMiddle}</div>);
+        rankingGrid.push(<div className="rank-row bottom-tier">{cellCollectionBottom}</div>);
+        rankingGrid.push(<div className="rank-row worst-tier">{cellCollectionWorst}</div>);
+
+        return rankingGrid;
+    }
+
+    function createRankingGrid() {
+        createCellsForRows(); // fill cellCollections first
+        return createRowsForGrid(); // create rows containing of them
+    }
+    return (
+        <div className="rankings">
+            {createRankingGrid()}
+        </div>
+    )
+}
+
+export default RankingGrid;
